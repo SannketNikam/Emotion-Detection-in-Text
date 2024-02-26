2e1ea531eeeedabfcac87acb3a6f5bef6886c880
Sanket Nikam <77570082+SannketNikam@users.noreply.github.com>
2024-02-26 06:28:14 +0000

    Changed the time zone to IST

--- app.py
+++ app.py
@@ -1,135 +1,128 @@
-# Core Packages
 import streamlit as st
 import altair as alt
 import plotly.express as px
-
-# EDA Packages
 import pandas as pd
 import numpy as np
 from datetime import datetime
+import joblib
+from track_utils import create_page_visited_table, add_page_visited_details, view_all_page_visited_details, add_prediction_details, view_all_prediction_details, create_emotionclf_table, IST  # Import IST from track_utils
 
 # Load Model
-import joblib 
-pipe_lr = joblib.load(open("./models/emotion_classifier_pipe_lr.pkl","rb"))
-
-# Track Utils
-from track_utils import create_page_visited_table, add_page_visited_details, view_all_page_visited_details, add_prediction_details, view_all_prediction_details, create_emotionclf_table
+pipe_lr = joblib.load(open("./models/emotion_classifier_pipe_lr.pkl", "rb"))
 
 # Function
 def predict_emotions(docx):
-	results = pipe_lr.predict([docx])
-	return results[0]
+    results = pipe_lr.predict([docx])
+    return results[0]
 
 def get_prediction_proba(docx):
-	results = pipe_lr.predict_proba([docx])
-	return results
+    results = pipe_lr.predict_proba([docx])
+    return results
 
-emotions_emoji_dict = {"anger" : "😠", "disgust" : "🤮", "fear" : "😨😱", "happy" : "🤗", "joy" : "😂", "neutral" : "😐", "sad" : "😔", "sadness" : "😔", "shame" : "😳", "surprise" : "😮"}
+emotions_emoji_dict = {"anger": "😠", "disgust": "🤮", "fear": "😨😱", "happy": "🤗", "joy": "😂", "neutral": "😐", "sad": "😔", "sadness": "😔", "shame": "😳", "surprise": "😮"}
 
 # Main Application
 def main():
-	st.title("Emotion Classifier App")
-	menu = ["Home", "Monitor", "About"]
-	choice = st.sidebar.selectbox("Menu", menu)
-	create_page_visited_table()
-	create_emotionclf_table()
-	if choice == "Home":
-		add_page_visited_details("Home", datetime.now())
-		st.subheader("Emotion Detection in Text")
+    st.title("Emotion Classifier App")
+    menu = ["Home", "Monitor", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+    create_page_visited_table()
+    create_emotionclf_table()
+    if choice == "Home":
+        add_page_visited_details("Home", datetime.now(IST))
+        st.subheader("Emotion Detection in Text")
 
-		with st.form(key='emotion_clf_form'):
-			raw_text = st.text_area("Type Here")
-			submit_text = st.form_submit_button(label = 'Submit')
+        with st.form(key='emotion_clf_form'):
+            raw_text = st.text_area("Type Here")
+            submit_text = st.form_submit_button(label='Submit')
 
-		if submit_text:
-			col1, col2  = st.columns(2)
+        if submit_text:
+            col1, col2 = st.columns(2)
 
-			# Apply Function Here
-			prediction = predict_emotions(raw_text)
-			probability = get_prediction_proba(raw_text)
-			
-			add_prediction_details(raw_text, prediction, np.max(probability), datetime.now())
+            prediction = predict_emotions(raw_text)
+            probability = get_prediction_proba(raw_text)
 
-			with col1:
-				st.success("Original Text")
-				st.write(raw_text)
+            add_prediction_details(raw_text, prediction, np.max(probability), datetime.now(IST))
 
-				st.success("Prediction")
-				emoji_icon = emotions_emoji_dict[prediction]
-				st.write("{}:{}".format(prediction, emoji_icon))
-				st.write("Confidence:{}".format(np.max(probability)))
+            with col1:
+                st.success("Original Text")
+                st.write(raw_text)
 
-			with col2:
-				st.success("Prediction Probability")
-				# st.write(probability)
-				proba_df = pd.DataFrame(probability, columns = pipe_lr.classes_)
-				# st.write(proba_df.T)
-				proba_df_clean = proba_df.T.reset_index()
-				proba_df_clean.columns = ["emotions", "probability"]
+                st.success("Prediction")
+                emoji_icon = emotions_emoji_dict[prediction]
+                st.write("{}:{}".format(prediction, emoji_icon))
+                st.write("Confidence:{}".format(np.max(probability)))
 
-				fig = alt.Chart(proba_df_clean).mark_bar().encode(x = 'emotions', y = 'probability', color = 'emotions')
-				st.altair_chart(fig,use_container_width = True)
+            with col2:
+                st.success("Prediction Probability")
+                proba_df = pd.DataFrame(probability, columns=pipe_lr.classes_)
+                proba_df_clean = proba_df.T.reset_index()
+                proba_df_clean.columns = ["emotions", "probability"]
 
-	elif choice == "Monitor":
-		add_page_visited_details("Monitor", datetime.now())
-		st.subheader("Monitor App")
+                fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability', color='emotions')
+                st.altair_chart(fig, use_container_width=True)
 
-		with st.expander("Page Metrics"):
-			page_visited_details = pd.DataFrame(view_all_page_visited_details(), columns = ['Page Name', 'Time of Visit'])
-			st.dataframe(page_visited_details)	
+    elif choice == "Monitor":
+        add_page_visited_details("Monitor", datetime.now(IST))
+        st.subheader("Monitor App")
 
-			pg_count = page_visited_details['Page Name'].value_counts().rename_axis('Page Name').reset_index(name = 'Counts')
-			c = alt.Chart(pg_count).mark_bar().encode(x = 'Page Name', y = 'Counts', color = 'Page Name')
-			st.altair_chart(c,use_container_width = True)	
+        with st.expander("Page Metrics"):
+            page_visited_details = pd.DataFrame(view_all_page_visited_details(), columns=['Page Name', 'Time of Visit'])
+            st.dataframe(page_visited_details)
 
-			p = px.pie(pg_count,values='Counts', names = 'Page Name')
-			st.plotly_chart(p, use_container_width = True)
+            pg_count = page_visited_details['Page Name'].value_counts().rename_axis('Page Name').reset_index(name='Counts')
+            c = alt.Chart(pg_count).mark_bar().encode(x='Page Name', y='Counts', color='Page Name')
+            st.altair_chart(c, use_container_width=True)
 
-		with st.expander('Emotion Classifier Metrics'):
-			df_emotions = pd.DataFrame(view_all_prediction_details(), columns = ['Rawtext', 'Prediction', 'Probability', 'Time_of_Visit'])
-			st.dataframe(df_emotions)
+            p = px.pie(pg_count, values='Counts', names='Page Name')
+            st.plotly_chart(p, use_container_width=True)
 
-			prediction_count = df_emotions['Prediction'].value_counts().rename_axis('Prediction').reset_index(name = 'Counts')
-			pc = alt.Chart(prediction_count).mark_bar().encode(x = 'Prediction', y = 'Counts', color = 'Prediction')
-			st.altair_chart(pc, use_container_width = True)	
+        with st.expander('Emotion Classifier Metrics'):
+            df_emotions = pd.DataFrame(view_all_prediction_details(), columns=['Rawtext', 'Prediction', 'Probability', 'Time_of_Visit'])
+            st.dataframe(df_emotions)
 
-	else:
-		add_page_visited_details("About", datetime.now())
+            prediction_count = df_emotions['Prediction'].value_counts().rename_axis('Prediction').reset_index(name='Counts')
+            pc = alt.Chart(prediction_count).mark_bar().encode(x='Prediction', y='Counts', color='Prediction')
+            st.altair_chart(pc, use_container_width=True)
 
-		st.write("Welcome to the Emotion Detection in Text App! This application utilizes the power of natural language processing and machine learning to analyze and identify emotions in textual data.")
-		
-		st.subheader("Our Mission")
+    else:
+        add_page_visited_details("About", datetime.now(IST))
 
-		st.write("At Emotion Detection in Text, our mission is to provide a user-friendly and efficient tool that helps individuals and organizations understand the emotional content hidden within text. We believe that emotions play a crucial role in communication, and by uncovering these emotions, we can gain valuable insights into the underlying sentiments and attitudes expressed in written text.")
-		
-		st.subheader("How It Works")
-		
-		st.write("When you input text into the app, our system processes it and applies advanced natural language processing algorithms to extract meaningful features from the text. These features are then fed into the trained model, which predicts the emotions associated with the input text. The app displays the detected emotions, along with a confidence score, providing you with valuable insights into the emotional content of your text.")
-		
-		st.subheader("Key Features:")
-		
-		st.markdown("##### 1. Real-time Emotion Detection")
-		
-		st.write("Our app offers real-time emotion detection, allowing you to instantly analyze the emotions expressed in any given text. Whether you're analyzing customer feedback, social media posts, or any other form of text, our app provides you with immediate insights into the emotions underlying the text.")
-		
-		st.markdown("##### 2. Confidence Score")
-		
-		st.write("Alongside the detected emotions, our app provides a confidence score, indicating the model's certainty in its predictions. This score helps you gauge the reliability of the emotion detection results and make more informed decisions based on the analysis.")
-		
-		st.markdown("##### 3. User-friendly Interface")
-		
-		st.write("We've designed our app with simplicity and usability in mind. The intuitive user interface allows you to effortlessly input text, view the results, and interpret the emotions detected. Whether you're a seasoned data scientist or someone with limited technical expertise, our app is accessible to all.")
-		
-		st.subheader("Applications")
-		
-		st.markdown("""
-	      The Emotion Detection in Text App has a wide range of applications across various industries and domains. Some common use cases include:
-	      - Social media sentiment analysis
-	      - Customer feedback analysis
-	      - Market research and consumer insights
-	      - Brand monitoring and reputation management
-	      - Content analysis and recommendation systems
-	      """)
-		
+        st.write("Welcome to the Emotion Detection in Text App! This application utilizes the power of natural language processing and machine learning to analyze and identify emotions in textual data.")
+
+        st.subheader("Our Mission")
+
+        st.write("At Emotion Detection in Text, our mission is to provide a user-friendly and efficient tool that helps individuals and organizations understand the emotional content hidden within text. We believe that emotions play a crucial role in communication, and by uncovering these emotions, we can gain valuable insights into the underlying sentiments and attitudes expressed in written text.")
+
+        st.subheader("How It Works")
+
+        st.write("When you input text into the app, our system processes it and applies advanced natural language processing algorithms to extract meaningful features from the text. These features are then fed into the trained model, which predicts the emotions associated with the input text. The app displays the detected emotions, along with a confidence score, providing you with valuable insights into the emotional content of your text.")
+
+        st.subheader("Key Features:")
+
+        st.markdown("##### 1. Real-time Emotion Detection")
+
+        st.write("Our app offers real-time emotion detection, allowing you to instantly analyze the emotions expressed in any given text. Whether you're analyzing customer feedback, social media posts, or any other form of text, our app provides you with immediate insights into the emotions underlying the text.")
+
+        st.markdown("##### 2. Confidence Score")
+
+        st.write("Alongside the detected emotions, our app provides a confidence score, indicating the model's certainty in its predictions. This score helps you gauge the reliability of the emotion detection results and make more informed decisions based on the analysis.")
+
+        st.markdown("##### 3. User-friendly Interface")
+
+        st.write("We've designed our app with simplicity and usability in mind. The intuitive user interface allows you to effortlessly input text, view the results, and interpret the emotions detected. Whether you're a seasoned data scientist or someone with limited technical expertise, our app is accessible to all.")
+
+        st.subheader("Applications")
+
+        st.markdown("""
+          The Emotion Detection in Text App has a wide range of applications across various industries and domains. Some common use cases include:
+          - Social media sentiment analysis
+          - Customer feedback analysis
+          - Market research and consumer insights
+          - Brand monitoring and reputation management
+          - Content analysis and recommendation systems
+          """)
+
+
 if __name__ == '__main__':
-	main()
+    main()
--- track_utils.py
+++ track_utils.py
@@ -1,31 +1,47 @@
-# Load Database Packages
 import sqlite3
-# Creates database in the directory
-conn = sqlite3.connect('./data/data.db', check_same_thread = False)
+import pytz
+from datetime import datetime
+
+# Load Database Packages
+conn = sqlite3.connect('./data/data.db', check_same_thread=False)
 c = conn.cursor()
 
-# Function
+IST = pytz.timezone('Asia/Kolkata')  # Indian Standard Time
+
+# Function to create page visited table
 def create_page_visited_table():
-	c.execute('CREATE TABLE IF NOT EXISTS pageTrackTable(pagename TEXT, timeOfvisit TIMESTAMP)')
+    c.execute('CREATE TABLE IF NOT EXISTS pageTrackTable(pagename TEXT, timeOfvisit TIMESTAMP)')
 
-def add_page_visited_details(pagename, timeOfvisit):
-	c.execute('INSERT INTO pageTrackTable(pagename, timeOfvisit) VALUES(?, ?)', (pagename, timeOfvisit))
-	conn.commit()
+# Function to add page visited details
+def add_page_visited_details(pagename, timeOfvisit=None):
+    if timeOfvisit is None:
+        timeOfvisit = datetime.now(IST).strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        timeOfvisit = timeOfvisit.astimezone(IST).strftime("%Y-%m-%d %H:%M:%S")
+    c.execute('INSERT INTO pageTrackTable(pagename, timeOfvisit) VALUES (?, ?)', (pagename, timeOfvisit))
+    conn.commit()
 
+# Function to view all page visited details
 def view_all_page_visited_details():
-	c.execute('SELECT * FROM pageTrackTable')
-	data = c.fetchall()
-	return data
+    c.execute('SELECT * FROM pageTrackTable')
+    data = c.fetchall()
+    return data
 
-# Function to Track Input & Prediction
+# Function to create emotion classifier table
 def create_emotionclf_table():
-	c.execute('CREATE TABLE IF NOT EXISTS emotionclfTable(rawtext TEXT, prediction TEXT, probability NUMBER, timeOfvisit TIMESTAMP)')
+    c.execute('CREATE TABLE IF NOT EXISTS emotionclfTable(rawtext TEXT, prediction TEXT, probability NUMBER, timeOfvisit TIMESTAMP)')
 
-def add_prediction_details(rawtext, prediction, probability, timeOfvisit):
-	c.execute('INSERT INTO emotionclfTable(rawtext, prediction, probability, timeOfvisit) VALUES(?, ?, ?, ?)',(rawtext, prediction, probability, timeOfvisit))
-	conn.commit()
+# Function to add prediction details
+def add_prediction_details(rawtext, prediction, probability, timeOfvisit=None):
+    if timeOfvisit is None:
+        timeOfvisit = datetime.now(IST).strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        timeOfvisit = timeOfvisit.astimezone(IST).strftime("%Y-%m-%d %H:%M:%S")
+    c.execute('INSERT INTO emotionclfTable(rawtext, prediction, probability, timeOfvisit) VALUES (?, ?, ?, ?)', (rawtext, prediction, probability, timeOfvisit))
+    conn.commit()
 
+# Function to view all prediction details
 def view_all_prediction_details():
-	c.execute('SELECT * FROM emotionclfTable')
-	data = c.fetchall()
-	return data
+    c.execute('SELECT * FROM emotionclfTable')
+    data = c.fetchall()
+    return data
